c908dadb8f2b38ddb7c37f0797e85ca13215e8e9
jblowe <jblowe@berkeley.edu>
2014-08-28 16:43:04 -0700

    BAMPFA-256: initial prototype of packing list

--- cspace_django_site/wsgi.py
+++ cspace_django_site/wsgi.py
@@ -27,7 +27,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", SITE_NAME + ".settings")
 # can detect the path for the WSGI's Apache mount point.  This value must
 # correspond the the value used to set Apache's WSGIScriptAlias mount point.
 #
-WSGI_BASE = '/webapps'
+WSGI_BASE = '/bampfa_project'
 os.environ.setdefault(SITE_NAME + ".WSGI_BASE", WSGI_BASE)
 
 # This application object is used by any WSGI server configured to use this
