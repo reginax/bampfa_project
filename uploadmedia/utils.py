9e54c614293693581c45c1c8f9fc8f3f9ee931fd
John Lowe <jblowe@berkeley.edu>
2015-02-27 13:58:30 -0800

    BAMPFA-386: code cleanup

--- uploadmedia/utils.py
+++ uploadmedia/utils.py
@@ -112,20 +112,24 @@ objectnumberpattern = re.compile('([a-z]+)\.([a-zA-Z0-9]+)')
 def getNumber(filename):
     imagenumber = ''
     # the following is only for bampfa filenames...
+    # input is something like: bampfa_1995-46-194-a-199.jpg, output should be: 1995.46.194.a-199
     if 'bampfa_' in filename:
         objectnumber = filename.replace('bampfa_', '')
         try:
             objectnumber, imagenumber, imagetype = objectnumber.split('_')
         except:
             imagenumber = '1'
+        # these legacy statement retained, just in case...
         #numHyphens = objectnumber.count("-") - 1
         #objectnumber = objectnumber.replace('-', '.', numHyphens)
         objectnumber = objectnumber.replace('-', '.')
         objectnumber = objectnumberpattern.sub(r'\1-\2',objectnumber)
+    # for non-bampfa users (i.e. pahma, at the moment) it suffices to split on underscore...
     else:
         objectnumber = filename
         objectnumber = objectnumber.split('_')[0]
-    objectnumber = objectnumber.replace('.JPG', '').replace('.jpg', '')
+    # the following is a last ditch attempt to get an object number from a filename...
+    objectnumber = objectnumber.replace('.JPG', '').replace('.jpg', '').replace('.TIF','').replace('.tif','')
     return filename, objectnumber, imagenumber
 
 
